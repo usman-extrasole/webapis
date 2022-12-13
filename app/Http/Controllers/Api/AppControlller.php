d673c42c27cd5c3a311cb48558ed1ee5339c48fd
Mughal Usman <usman.mughal@extrasol.co.uk>
2022-12-14 01:43:08 +0500

    All done

---
+++ app/Http/Controllers/Api/AppControlller.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\About;
+use App\Category;
+use App\ChildCategory;
+use App\Contact;
+use App\Donate;
+use App\Http\Controllers\Controller;
+use App\Privacy;
+use App\SubCatgeory;
+use Illuminate\Http\Request;
+
+class AppControlller extends Controller
+{
+    public $successStatus = 200;
+    public function libraryCategory()
+    {
+        $category = Category::where('status','=',0)->get();
+        return response()->json(['success' => $category], $this->successStatus);
+    }
+    public function librarySubCategory()
+    {
+        $subcategory = SubCatgeory::where('status','=',0)->get();
+        return response()->json(['success' => $subcategory], $this->successStatus);
+    }
+    public function libraryChildCategory()
+    {
+        $childcategory = ChildCategory::where('status','=',0)->get();
+        return response()->json(['success' => $childcategory], $this->successStatus);
+    }
+    public function guitarCategory()
+    {
+        $category = Category::where('status','=',1)->get();
+        return response()->json(['success' => $category], $this->successStatus);
+    }
+    public function guitarSubCategory()
+    {
+        $subcategory = SubCatgeory::where('status','=',1)->get();
+        return response()->json(['success' => $subcategory], $this->successStatus);
+    }
+    public function guitarChildCategory()
+    {
+        $childcategory = ChildCategory::where('status','=',1)->get();
+        return response()->json(['success' => $childcategory], $this->successStatus);
+    }
+    public function pianoCategory()
+    {
+        $category = Category::where('status','=',2)->get();
+        return response()->json(['success' => $category], $this->successStatus);
+    }
+    public function pianoSubCategory()
+    {
+        $subcategory = SubCatgeory::where('status','=',2)->get();
+        return response()->json(['success' => $subcategory], $this->successStatus);
+    }
+    public function pianoChildCategory()
+    {
+        $childcategory = ChildCategory::where('status','=',2)->get();
+        return response()->json(['success' => $childcategory], $this->successStatus);
+    }
+    public function contact()
+    {
+        $contact = Contact::all();
+        return response()->json(['success' => $contact], $this->successStatus);
+    }
+    public function about()
+    {
+        $about = About::all();
+        return response()->json(['success' => $about], $this->successStatus);
+    }
+    public function privacy()
+    {
+        $privacy = Privacy::all();
+        return response()->json(['success' => $privacy], $this->successStatus);
+    }
+    public function donate()
+    {
+        $donate = Donate::all();
+        return response()->json(['success' => $donate], $this->successStatus);
+    }
+    public function librarySearch(Request $request)
+    {
+        $search = ChildCategory::where('status','=',0)->where('name1', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name2', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name3', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name4', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name5', 'LIKE', '%' . $request->keyword . '%')
+            ->get();
+        return response()->json(['success' => $search], $this->successStatus);
+    }
+    public function guitarSearch(Request $request)
+    {
+        $search = ChildCategory::where('status','=',1)->where('name1', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name2', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name3', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name4', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name5', 'LIKE', '%' . $request->keyword . '%')
+            ->get();
+        return response()->json(['success' => $search], $this->successStatus);
+    }
+    public function pianoSearch(Request $request)
+    {
+        $search = ChildCategory::where('status','=',2)->where('name1', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name2', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name3', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name4', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('name5', 'LIKE', '%' . $request->keyword . '%')
+            ->get();
+        return response()->json(['success' => $search], $this->successStatus);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('library/category', 'Api\AppControlller@libraryCategory');
+Route::get('library/subcategory', 'Api\AppControlller@librarySubCategory');
+Route::get('library/childcategory', 'Api\AppControlller@libraryChildCategory');
+Route::post('library/search', 'Api\AppControlller@librarySearch');
+
+Route::get('guitar/category', 'Api\AppControlller@guitarCategory');
+Route::get('guitar/subcategory', 'Api\AppControlller@guitarSubCategory');
+Route::get('guitar/childcategory', 'Api\AppControlller@guitarChildCategory');
+Route::post('guitar/search', 'Api\AppControlller@guitarSearch');
+
+Route::get('piano/category', 'Api\AppControlller@pianoCategory');
+Route::get('piano/subcategory', 'Api\AppControlller@pianoSubCategory');
+Route::get('piano/childcategory', 'Api\AppControlller@pianoChildCategory');
+Route::post('piano/search', 'Api\AppControlller@pianoSearch');
+
+Route::get('contact', 'Api\AppControlller@contact');
+Route::get('about', 'Api\AppControlller@about');
+Route::get('privacy', 'Api\AppControlller@privacy');
+Route::get('donate ', 'Api\AppControlller@donate');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
