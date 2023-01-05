f54a603109ef7fb80921b10e1278fb05ec2e6c24
Muhammad  Usman <usman.mughal@extrasol.co.uk>
2023-01-05 14:57:28 +0500

    All done

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 </div>
 <footer class="main-footer">
-    <strong>Copyright &copy; 2022-2070 <a href="https://adminlte.io">Admin Dashboard</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="#">Extrasol</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
